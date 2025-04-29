e8d94aa9c3986f6e982141b96819dd86334bc777
Wren <kirliakat1000@gmail.com>
2025-04-29 16:14:12 +0100

    commit

--- src/main/java/com/Wren/ToleranceTrackerConfig.java
+++ src/main/java/com/Wren/ToleranceTrackerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.Wren;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface ToleranceTrackerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
